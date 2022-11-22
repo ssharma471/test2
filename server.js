bbdc3b7b176691a5482c91856a8e4502ea807356
sidhantss <sidhantsharmavlog@gmail.com>
2022-11-22 16:46:00 -0500

    ssss

--- server.js
+++ server.js
@@ -20,11 +20,11 @@ app.get("/", (req, res) => {
     // " Name: <b> <mark>Sidhant Sharma </mark> </b> <br><br> Student Number: <b> <mark>123151219 </mark> <b> <br><br> <a href='/BSD'>Click to visit BSD Students</a><br> <a href='/highGPA'>Click to see who has the highest GPA</a><br>")
 });
 
-app.get("/BSD", (req, res) => {
-  res.render('./layouts/home');
-    // res.send("<h2>Declaration (text size in heading 2):</h2><p>The rest text is displayed in paragraph as shown in screenshot.<br>I acknowledge the college's academic intergrity policy - and my own intergrity - remain in effect whether my work is done remotely or onsite. Any test or assignment is an act of trust between me and my instructor, and especially with my classmates... even when no one is watching. I declare I will not break the trust.</p>"+
-    // " Name: <b> <mark>Sidhant Sharma </mark> </b> <br><br> Student Number: <b> <mark>123151219 </mark> <b> <br><br> <a href='/BSD'>Click to visit BSD Students</a><br> <a href='/highGPA'>Click to see who has the highest GPA</a><br>")
-});
+// app.get("/", (req, res) => {
+//   res.render('./layouts/home');
+//     // res.send("<h2>Declaration (text size in heading 2):</h2><p>The rest text is displayed in paragraph as shown in screenshot.<br>I acknowledge the college's academic intergrity policy - and my own intergrity - remain in effect whether my work is done remotely or onsite. Any test or assignment is an act of trust between me and my instructor, and especially with my classmates... even when no one is watching. I declare I will not break the trust.</p>"+
+//     // " Name: <b> <mark>Sidhant Sharma </mark> </b> <br><br> Student Number: <b> <mark>123151219 </mark> <b> <br><br> <a href='/BSD'>Click to visit BSD Students</a><br> <a href='/highGPA'>Click to see who has the highest GPA</a><br>")
+// });
 app.get('/BSD', function(req, res) {
     data.getBSD().then((data) => {
         var bsd = JSON.stringify(data);
@@ -41,12 +41,7 @@ app.get('/BSD', function(req, res) {
     });
 
   });
-  app.get("./layouts/stduents", (req, res) => {
-      res.render("stduents");
-      //res.json(dataservice);
-    }).catch(err=> res.render({message:"no results"}));
-  
-  
+
   app.use((req, res) => {
     res.status(404).send("Sorry this page does not loaded");
   });
@@ -58,5 +53,3 @@ app.get('/BSD', function(req, res) {
   }).catch(function(err){
     console.log("Unable to start server: " + err);
   });
-
-  
